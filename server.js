db4b53c244c31f29c804527b3a0d20c0e33457ee
Extain <trikmaniac@gmail.com>
2021-04-22 13:17:45 -0700

    add user model and connect to db

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const bodyParser= require('body-parser');
+const User= require('./models/User');
+
+mongoose.connect('mongodb://localhost:27017/userData');
+
 const port=8000;
 const app= express();
 
